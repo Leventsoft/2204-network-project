e6f9199b41521e38590256c8f27bc08ab28d437c
Levent <leventsarioglu02@gmail.com>
2024-04-24 12:06:27 +0300

    created-discovery thread

--- main/main.py
+++ main/main.py
@@ -6,9 +6,10 @@ broadcast_ip = "192.168.194.255"
 
 # Create a new thread
 Announcer_Thread = threading.Thread(target=Service_Announcer, args=(broadcast_ip,))
-
+Discovery_Thread = threading.Thread(target=Peer_Discovery)
 # Start the thread
 Announcer_Thread.start()
+Discovery_Thread.start()
 
 
 # Continue with the rest of your code
